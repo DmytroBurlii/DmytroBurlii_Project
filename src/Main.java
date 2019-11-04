4608689296ec1d604aefc9a3d47c81dcff15ec8d
Dmytro <dimaburliy@yahoo.com.ua>
2019-11-04 17:49:15 +0200

    Project

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         //TicTacToe.main(null);
         //Calculator.main(null);
         //BinSearch.main(null);
+        //InsertionSort.main(null);
+        //BubbleSort.main(null);
     }
 }
